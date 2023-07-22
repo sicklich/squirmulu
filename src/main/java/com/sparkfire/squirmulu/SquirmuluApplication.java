1e8c59ca3717038b9d961bb53112eb4719eab41b
yutonghua <yutonghua@tencent.com>
2023-07-22 18:50:48 +0800

    first commit

---
+++ src/main/java/com/sparkfire/squirmulu/SquirmuluApplication.java
@@ -0,0 +1,13 @@
+package com.sparkfire.squirmulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquirmuluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquirmuluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparkfire/squirmulu/SquirmuluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkfire.squirmulu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquirmuluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
