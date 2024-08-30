9730cf23ea90b39629e354ef233556000585c551
yutonghua <yutonghua@tencent.com>
2024-08-30 20:19:28 +0800

    埋点

---
+++ src/main/java/com/sparkfire/squirmulu/entity/TrackStay.java
@@ -0,0 +1,107 @@
+package com.sparkfire.squirmulu.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TrackStay {
+
+    private Long id;
+
+    @JsonProperty("track_type")
+    private Integer trackType;
+
+    @JsonProperty("event")
+    private Integer event;
+
+    @JsonProperty("url")
+    private String url;
+
+    @JsonProperty("timestamp")
+    private Long timestamp;
+
+    @JsonProperty("user_id")
+    private Long userId;
+
+    @JsonProperty("session")
+    private String session;
+
+    @JsonProperty("duration")
+    private String duration;
+
+    @JsonProperty("remark")
+    private String remark;
+
+    // Getters and Setters
+    // ...
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getTrackType() {
+        return trackType;
+    }
+
+    public void setTrackType(Integer trackType) {
+        this.trackType = trackType;
+    }
+
+    public Integer getEvent() {
+        return event;
+    }
+
+    public void setEvent(Integer event) {
+        this.event = event;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
